9bd71d4f9cdebb496bc4d9a666b15f5aaaa60ee2
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-08-16 23:11:11 +0200

    EWPP-40: Fix query type and add additional setting for widget.

--- src/Plugin/facets/query_type/Date.php
+++ src/Plugin/facets/query_type/Date.php
@@ -4,6 +4,10 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_list_pages\Plugin\facets\query_type;
 
+use Drupal\Component\Datetime\DateTimePlus;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItem;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 use Drupal\facets\QueryType\QueryTypePluginBase;
 
 /**
@@ -17,8 +21,8 @@ use Drupal\facets\QueryType\QueryTypePluginBase;
 class Date extends QueryTypePluginBase {
 
   const OPERATORS = [
-    'lt' => '<',
     'gt' => '>',
+    'lt' => '<',
     'bt' => 'BETWEEN',
   ];
 
@@ -29,20 +33,31 @@ class Date extends QueryTypePluginBase {
     $query = $this->query;
 
     // Only alter the query when there's an actual query object to alter.
-    if (!empty($query)) {
-      if (empty($this->facet->getActiveItems()[0])) {
-        return;
-      }
-      // Add the filter to the query if there are active values.
-      $active_items = $this->facet->getActiveItems();
-      if (count($active_items) && isset($active_items[0]) && isset($active_items[1])) {
-        $operator = self::OPERATORS[$active_items[0]];
-        $value = strtotime($active_items[1]);
-        if ($operator === 'BETWEEN' && isset($active_items[2])) {
-          $value = [$value, strtotime($active_items[2])];
+    if (empty($query) || empty($this->facet->getActiveItems()[0])) {
+      return;
+    }
+    $widget_config = $this->facet->getWidgetInstance()->getConfiguration();
+
+    // Add the filter to the query if there are active values.
+    $active_items = $this->facet->getActiveItems();
+    if (isset($active_items[1])) {
+      $operator = self::OPERATORS[$active_items[0]];
+      $timezone = new \DateTimeZone(DateTimeItemInterface::STORAGE_TIMEZONE);
+      $datetime = new DateTimePlus($active_items[1], $timezone);
+      if ($operator === 'BETWEEN' && isset($active_items[2])) {
+        $end_datetime = new DrupalDateTime($active_items[2], $timezone);
+        if ($widget_config['date_type'] === DateTimeItem::DATETIME_TYPE_DATE) {
+          $this->adaptDatesPerOperator($operator, $datetime, $end_datetime);
         }
-        $query->addCondition($this->facet->getFieldIdentifier(), $value, $operator);
+        $value = [$datetime->getTimestamp(), $end_datetime->getTimestamp()];
       }
+      else {
+        if ($widget_config['date_type'] === DateTimeItem::DATETIME_TYPE_DATE) {
+          $this->adaptDatesPerOperator($operator, $datetime);
+        }
+        $value = $datetime->getTimestamp();
+      }
+      $query->addCondition($this->facet->getFieldIdentifier(), $value, $operator);
     }
   }
 
@@ -51,6 +66,35 @@ class Date extends QueryTypePluginBase {
    */
   public function build() {
     return $this->facet;
+  }
+
+  /**
+   * Adapt date per operator.
+   *
+   * @param string $operator
+   *   The operator.
+   * @param \Drupal\Component\Datetime\DateTimePlus $start_date
+   *   The start date.
+   * @param \Drupal\Component\Datetime\DateTimePlus|null $end_date
+   *   The end date.
+   */
+  protected function adaptDatesPerOperator(string $operator, DateTimePlus &$start_date, DateTimePlus &$end_date = NULL): void {
+    switch ($operator) {
+      case '>':
+        // Next day after selected day.
+        $start_date->setTime(23, 59, 59);
+        break;
+
+      case '<':
+        // Previous day.
+        $start_date->setTime(0, 0, 0);
+        break;
+
+      case 'BETWEEN':
+        $start_date->setTime(0, 0, 0);
+        $end_date->setTime(23, 59, 59);
+        break;
+    }
   }
 
 }
--- src/Plugin/facets/widget/ListPagesDateWidget.php
+++ src/Plugin/facets/widget/ListPagesDateWidget.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\oe_list_pages\Plugin\facets\widget;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItem;
 use Drupal\facets\FacetInterface;
 
 /**
@@ -13,7 +14,7 @@ use Drupal\facets\FacetInterface;
  * @FacetsWidget(
  *   id = "oe_list_pages_date",
  *   label = @Translation("List pages date"),
- *   description = @Translation("A date search widget."),
+ *   description = @Translation("A date filter widget."),
  * )
  */
 class ListPagesDateWidget extends ListPagesWidgetBase {
@@ -22,6 +23,8 @@ class ListPagesDateWidget extends ListPagesWidgetBase {
    * {@inheritdoc}
    */
   public function build(FacetInterface $facet) {
+    $date_type = $facet->getWidgetInstance()->getConfiguration()['date_type'];
+
     $operators = [
       'gt' => $this->t('Greater than'),
       'lt' => $this->t('Lower than'),
@@ -36,18 +39,26 @@ class ListPagesDateWidget extends ListPagesWidgetBase {
     ];
 
     $build[$facet->id() . '_date'] = [
-      '#type' => 'date',
+      '#type' => $date_type,
       '#title' => $this->t('Date'),
-      '#format' => 'm/d/Y',
       '#default_value' => $this->getValueFromActiveFilters($facet, '1'),
     ];
 
+    // In any cases, we are going to ignore value of end_date,
+    // if operator is not 'In between'.
+    $end_date_value = NULL;
+    if ($this->getValueFromActiveFilters($facet, '0') === 'bt') {
+      $end_date_value = $this->getValueFromActiveFilters($facet, '2') ?? NULL;
+    }
+
     $build[$facet->id() . '_end_date'] = [
-      '#type' => 'date',
+      '#type' => $date_type,
       '#title' => $this->t('End Date'),
-      '#format' => 'm/d/Y',
-      '#default_value' => $this->getValueFromActiveFilters($facet, '2') ?? NULL,
+      '#default_value' => $end_date_value,
       '#states' => [
+        // Doesn't work for form element datetime.
+        // @todo Fix or prepare patch for datetime type.
+        // @see https://www.drupal.org/project/drupal/issues/2419131
         'visible' => [
           ':input[name="' . $facet->id() . '_op"]' => [
             'value' => 'bt',
@@ -89,6 +100,33 @@ class ListPagesDateWidget extends ListPagesWidgetBase {
    */
   public function getQueryType() {
     return 'date_comparison';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'date_type' => DateTimeItem::DATETIME_TYPE_DATE,
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state, FacetInterface $facet) {
+    $form['date_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Date type'),
+      '#options' => [
+        DateTimeItem::DATETIME_TYPE_DATE => $this->t('Date only'),
+        DateTimeItem::DATETIME_TYPE_DATETIME => $this->t('Date and time'),
+      ],
+      '#description' => $this->t('Choose the type of date to filter.'),
+      '#default_value' => $this->getConfiguration()['date_type'],
+    ];
+
+    return $form;
   }
 
 }
