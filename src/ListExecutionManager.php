b34eb1d4d35653454389d5b34a3ffda56cec08ed
Hernâni <hernaniborgesdefreitas@gmail.com>
2020-09-08 15:57:04 +0100

    EWPP-220: Third party settings for list execution manager.

--- src/ListExecutionManager.php
+++ src/ListExecutionManager.php
@@ -4,7 +4,11 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_list_pages;
 
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Pager\PagerManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -27,18 +31,52 @@ class ListExecutionManager implements ListExecutionManagerInterface {
   protected $requestStack;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The pager manager.
+   *
+   * @var \Drupal\Core\Pager\PagerManagerInterface
+   */
+  protected $pagerManager;
+
+  /**
+   * The pager.
+   *
+   * @var \Drupal\Core\Pager\PagerManagerInterface
+   */
+  protected $pager;
+
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
    * ListManager constructor.
    *
    * @param \Drupal\oe_list_pages\ListSourceFactoryInterface $listSourceFactory
    *   The list source factory.
    * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
    *   The entity type manager.
+   * @param \Drupal\Core\Pager\PagerManagerInterface $pager
+   *   The pager.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entityRepository
+   *   The entity repository.
    * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
    *   The request stack.
    */
-  public function __construct(ListSourceFactoryInterface $listSourceFactory, EntityTypeManager $entityTypeManager, RequestStack $requestStack) {
+  public function __construct(ListSourceFactoryInterface $listSourceFactory, EntityTypeManager $entityTypeManager, PagerManagerInterface $pager, EntityRepositoryInterface $entityRepository, RequestStack $requestStack) {
     $this->listSourceFactory = $listSourceFactory;
     $this->entityTypeManager = $entityTypeManager;
+    $this->pager = $pager;
+    $this->entityRepository = $entityRepository;
     $this->requestStack = $requestStack;
   }
 
@@ -62,13 +100,82 @@ class ListExecutionManager implements ListExecutionManagerInterface {
     $bundle_entity_type = $this->entityTypeManager->getDefinition($wrapper->getSourceEntityType())->getBundleEntityType();
     $storage = $this->entityTypeManager->getStorage($bundle_entity_type);
     $bundle = $storage->load($wrapper->getSourceEntityBundle());
-    $sort = $bundle->getThirdPartySetting('ewcms_site_tree', 'site_tree_pool_sort', []);
+    $sort = $bundle->getThirdPartySetting('oe_list_pages', 'default_sort', []);
     $current_page = (int) $this->requestStack->getCurrentRequest()->get('page', 0);
     $sort = $sort ? [$sort['name'] => $sort['direction']] : [];
     $query = $list_source->getQuery($limit, $current_page, $sort);
     $result = $query->execute();
     $listExecution = new ListExecution($query, $result, $list_source, $wrapper);
     return $listExecution;
+  }
+
+  /**
+   * Builds the list content to be rendered.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The content entity.
+   * @param \Drupal\oe_list_pages\ListExecution $listExecution
+   *   The list execution.
+   *
+   * @return array
+   *   The list render array.
+   */
+  public function buildList(ContentEntityInterface $entity, ListExecution $listExecution): array {
+    $build = [];
+
+    $cache = new CacheableMetadata();
+    $cache->addCacheTags($entity->getEntityType()->getListCacheTags());
+    $list_source = $listExecution->getListSource();
+    if (!$list_source) {
+      $cache->applyTo($build);
+      return $build;
+    }
+
+    $query = $listExecution->getQuery();
+    $result = $listExecution->getResults();
+    $wrapper = $listExecution->getListPluginWrapper();
+
+    // Determine the view mode to render with and the sorting.
+    $bundle_entity_type = $this->entityTypeManager->getDefinition($wrapper->getSourceEntityType())->getBundleEntityType();
+    $storage = $this->entityTypeManager->getStorage($bundle_entity_type);
+    $bundle = $storage->load($wrapper->getSourceEntityBundle());
+    $view_mode = $bundle->getThirdPartySetting('oe_list_pages', 'default_view_mode', 'teaser');
+    $cache->addCacheableDependency($query);
+    $cache->addCacheTags(['search_api_list:' . $query->getIndex()->id()]);
+
+    if (!$result->getResultCount()) {
+      $cache->applyTo($build);
+      return $build;
+    }
+
+    $this->pager->createPager($result->getResultCount(), $query->getOption('limit'));
+
+    $items = [];
+
+    // Build the entities.
+    $builder = $this->entityTypeManager->getViewBuilder($wrapper->getSourceEntityType());
+    foreach ($result->getResultItems() as $item) {
+      $entity = $item->getOriginalObject()->getEntity();
+      $cache->addCacheableDependency($entity);
+      $entity = $this->entityRepository->getTranslationFromContext($entity);
+      $items[] = $builder->view($entity, $view_mode);
+    }
+
+    $build['list'] = [
+      '#type' => 'pattern',
+      '#id' => 'list_item_block_one_column',
+      '#fields' => [
+        'items' => $items,
+      ],
+    ];
+
+    $build['pager'] = [
+      '#type' => 'pager',
+    ];
+
+    $cache->applyTo($build);
+
+    return $build;
   }
 
 }
