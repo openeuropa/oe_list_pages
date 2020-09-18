e5e9213561cd4536a8688065983f25450283e4ae
upchuk <danny@webomelette.com>
2020-09-18 09:28:49 +0200

    EWPP-221: Refactoring the date filter query and processing.

--- src/Plugin/facets/processor/DateUrlProcessor.php
+++ src/Plugin/facets/processor/DateUrlProcessor.php
@@ -4,8 +4,11 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_list_pages\Plugin\facets\processor;
 
+use Drupal\Component\Render\FormattableMarkup;
 use Drupal\facets\FacetInterface;
 use Drupal\facets\Plugin\facets\processor\UrlProcessorHandler;
+use Drupal\facets\Result\Result;
+use Drupal\oe_list_pages\Plugin\facets\query_type\Date;
 
 /**
  * The URL processor handler for adapting active filter for date fields.
@@ -26,61 +29,50 @@ class DateUrlProcessor extends UrlProcessorHandler {
   /**
    * {@inheritdoc}
    */
-  public function preQuery(FacetInterface $facet) {
-    $active_filters = $this->processor->getActiveFilters();
-    if (isset($active_filters[$facet->id()]) && $facet->getWidget()['type'] === 'oe_list_pages_date') {
-      // The date facet comes with an active filter in the form of gt|2020-08-21
-      // or bt|2020-08-21|2020-08-23. So we need to explode this and structure
-      // this information so that the query plugin and widget can better
-      // understand it.
-      $active_items = [];
-      // Normally we should only have one filter.
-      $active_filter_values = explode('|', $active_filters[$facet->id()][0]);
-      foreach ($active_filter_values as $value) {
-        $active_items[] = $value;
-      }
+  public function build(FacetInterface $facet, array $results) {
+    $facet_results = [];
 
-      $facet->setActiveItems($active_items);
-    }
-  }
-
-  /**
-   * Structure the active facet items into a descriptive array.
-   *
-   * NB: this needs to be called only after the preQuery() has run.
-   *
-   * @param \Drupal\facets\FacetInterface $facet
-   *   The facet.
-   *
-   * @return array
-   *   The structured array containing the operator and date values.
-   */
-  public static function structureActiveItems(FacetInterface $facet): array {
-    if (!$facet->getWidget()['type'] === 'oe_list_pages_date') {
-      return [];
+    if (!empty($results)) {
+      return $results;
     }
 
-    $active_filters = $facet->getActiveItems();
+    $active_filters = Date::getActiveItems($facet);
     if (!$active_filters) {
-      return [];
+      return $facet_results;
     }
 
-    if (!isset($active_filters[1])) {
-      // Normally should not happen, at least 1 filter value needs to be
-      // present so we behave as no filter exists.
-      return [];
-    }
+    $operator = $active_filters['operator'];
+    $first_date = $active_filters['first'];
+    $second_date = isset($active_filters['second']) ? $active_filters['second'] : NULL;
 
-    $items = [
-      'operator' => $active_filters[0],
-      'first' => $active_filters[1],
+    $operators = [
+      'gt' => $this->t('After'),
+      'lt' => $this->t('Before'),
+      'bt' => $this->t('Between'),
     ];
 
-    if (isset($active_filters[2])) {
-      $items['second'] = $active_filters[2];
+    if (!isset($operators[$operator])) {
+      return $facet_results;
     }
 
-    return $items;
+    if ($operator === 'bt') {
+      $display = new FormattableMarkup('@operator @first and @second', [
+        '@operator' => $operators[$operator],
+        '@first' => $first_date->format('d F Y'),
+        '@second' => $second_date->format('d F Y'),
+      ]);
+      $result = new Result($facet, $active_filters['_raw'], $display, 0);
+      $facet_results[] = $result;
+      return $facet_results;
+    }
+
+    $display = new FormattableMarkup('@operator @first', [
+      '@operator' => $operators[$operator],
+      '@first' => $first_date->format('d F Y'),
+    ]);
+    $result = new Result($facet, $active_filters['_raw'], $display, 0);
+    $facet_results[] = $result;
+    return $facet_results;
   }
 
 }
