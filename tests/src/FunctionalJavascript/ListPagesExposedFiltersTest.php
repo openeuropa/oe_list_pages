5224c51dfe349c80bdf2d1f2133759cb822efbf7
upchuk <danny@webomelette.com>
2020-09-21 13:23:17 +0200

    EWPP-223: Test cleanup.

--- tests/src/FunctionalJavascript/ListPagesExposedFiltersTest.php
+++ tests/src/FunctionalJavascript/ListPagesExposedFiltersTest.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_list_pages\FunctionalJavascript;
 
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
-use Drupal\node\Entity\Node;
 
 /**
  * Tests the List pages exposed filters.
@@ -79,80 +78,78 @@ class ListPagesExposedFiltersTest extends WebDriverTestBase {
 
     $page->pressButton('Save');
 
-    \Drupal::entityTypeManager()->getStorage('node')->resetCache();
     \Drupal::entityTypeManager()->getStorage('entity_meta_relation')->resetCache();
 
-    $node = Node::load(1);
+    $node = $this->drupalGetNodeByTitle('Node title', TRUE);
     /** @var \Drupal\emr\Field\ComputedEntityMetasItemList $entity_meta_list */
     $entity_meta_list = $node->get('emr_entity_metas');
     $entity_meta = $entity_meta_list->getEntityMeta('oe_list_page');
     $entity_meta_wrapper = $entity_meta->getWrapper();
     $actual_exposed_filters = $entity_meta_wrapper->getConfiguration()['exposed_filters'];
-    $this->assertEquals($actual_exposed_filters, [
+    $this->assertEquals([
       'list_facet_source_node_content_type_onestatus' => 'list_facet_source_node_content_type_onestatus',
       'select_one' => 'select_one',
-    ]);
+    ], $actual_exposed_filters);
 
-    $this->drupalGet('/node/1/edit');
+    $this->drupalGet($node->toUrl('edit-form'));
     $this->clickLink('List Page');
     $page->selectFieldOption('Source bundle', 'Content type two');
     $this->assertSession()->assertWaitOnAjaxRequest();
     $page->checkField('Select two');
     $page->pressButton('Save');
 
-    \Drupal::entityTypeManager()->getStorage('node')->resetCache();
     \Drupal::entityTypeManager()->getStorage('entity_meta_relation')->resetCache();
 
-    $node = Node::load(1);
+    $node = $this->drupalGetNodeByTitle('Node title', TRUE);
     /** @var \Drupal\emr\Field\ComputedEntityMetasItemList $entity_meta_list */
     $entity_meta_list = $node->get('emr_entity_metas');
     $entity_meta = $entity_meta_list->getEntityMeta('oe_list_page');
     $entity_meta_wrapper = $entity_meta->getWrapper();
     $actual_exposed_filters = $entity_meta_wrapper->getConfiguration()['exposed_filters'];
-    $this->assertEquals($actual_exposed_filters, [
+    $this->assertEquals([
       'list_facet_source_node_content_type_twofield_select_two' => 'list_facet_source_node_content_type_twofield_select_two',
-    ]);
+    ], $actual_exposed_filters);
 
-    $this->drupalGet('/node/1/edit');
+    $this->drupalGet($node->toUrl('edit-form'));
     $this->clickLink('List Page');
     $this->assertFieldChecked('Select two');
     $this->assertNoFieldChecked('Facet for status');
 
-    // Unselect all.
+    // Unselect all the exposed filters and assert that we have overridden
+    // the list page to not show any exposed filters.
     $page->uncheckField('Select two');
     $page->uncheckField('Facet for status');
     $page->pressButton('Save');
-    \Drupal::entityTypeManager()->getStorage('node')->resetCache();
     \Drupal::entityTypeManager()->getStorage('entity_meta_relation')->resetCache();
-    $node = Node::load(1);
+
+    $node = $this->drupalGetNodeByTitle('Node title', TRUE);
     /** @var \Drupal\emr\Field\ComputedEntityMetasItemList $entity_meta_list */
     $entity_meta_list = $node->get('emr_entity_metas');
     $entity_meta = $entity_meta_list->getEntityMeta('oe_list_page');
     $entity_meta_wrapper = $entity_meta->getWrapper();
     $actual_exposed_filters = $entity_meta_wrapper->getConfiguration()['exposed_filters'];
     $actual_override_exposed_filters = $entity_meta_wrapper->getConfiguration()['override_exposed_filters'];
-    $this->assertEquals($actual_override_exposed_filters, TRUE);
-    $this->assertEquals($actual_exposed_filters, []);
+    $this->assertEquals(TRUE, $actual_override_exposed_filters);
+    $this->assertEquals([], $actual_exposed_filters);
 
-    // Disable overriden exposed filters.
-    $this->drupalGet('/node/1/edit');
+    // Disable the overridden exposed filters to return back to the defaults.
+    $this->drupalGet($node->toUrl('edit-form'));
     $this->clickLink('List Page');
     $this->assertFieldChecked('Override default exposed filters');
     $this->assertNoFieldChecked('Select two');
     $this->assertNoFieldChecked('Facet for status');
     $page->uncheckField('Override default exposed filters');
     $page->pressButton('Save');
-    \Drupal::entityTypeManager()->getStorage('node')->resetCache();
     \Drupal::entityTypeManager()->getStorage('entity_meta_relation')->resetCache();
-    $node = Node::load(1);
+    $node = $this->drupalGetNodeByTitle('Node title', TRUE);
     /** @var \Drupal\emr\Field\ComputedEntityMetasItemList $entity_meta_list */
     $entity_meta_list = $node->get('emr_entity_metas');
     $entity_meta = $entity_meta_list->getEntityMeta('oe_list_page');
     $entity_meta_wrapper = $entity_meta->getWrapper();
     $actual_exposed_filters = $entity_meta_wrapper->getConfiguration()['exposed_filters'];
     $actual_override_exposed_filters = $entity_meta_wrapper->getConfiguration()['override_exposed_filters'];
-    $this->assertEquals($actual_override_exposed_filters, FALSE);
-    $this->assertEquals($actual_exposed_filters, []);
+    $this->assertEquals(FALSE, $actual_override_exposed_filters);
+    $this->assertEquals([], $actual_exposed_filters);
   }
 
   /**
