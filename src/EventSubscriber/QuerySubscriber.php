1097abe42d1ba0f626fae3456e59d53eb35bbd31
upchuk <danny@webomelette.com>
2020-11-24 16:01:12 +0100

    EWPP-306: Changing the preset filter logic.

--- src/EventSubscriber/QuerySubscriber.php
+++ src/EventSubscriber/QuerySubscriber.php
@@ -129,29 +129,19 @@ class QuerySubscriber implements EventSubscriberInterface {
     }
 
     foreach ($grouped_preset_filter_values as $facet_id => $values) {
-      if (count($values) === 1) {
-        // If we have only 1 filter for this facet, we just apply the operator
-        // and preset values on it.
-        $preset_filter = reset($values);
-        $filter_id = key($values);
-        $this->applyPresetFilterValues($facets[$filter_id], $preset_filter);
-        continue;
-      }
-
-      // Otherwise, we need to generate extra facets to accommodate multiple
-      // filters.
-      $default_filter_id = key($values);
+      // For each facet, we need to keep the original with the active values
+      // from the context, and clone it for each time it has been set as a
+      // preset filters.
+      $original_facet_filter_id = key($values);
       /** @var \Drupal\facets\Entity\Facet $original_facet */
-      $original_facet = $facets[$default_filter_id];
-      // Unset the original facet from the list, as will will clone it for each
-      // filter that uses this facet.
-      unset($facets[$default_filter_id]);
+      $original_facet = $facets[$original_facet_filter_id];
 
       foreach ($values as $preset_filter_id => $preset_filter) {
         $facet = clone $original_facet;
-
+        // Generate a new filter ID for each of the clone.
         $this->applyPresetFilterValues($facet, $preset_filter);
-        $facets[$preset_filter_id] = $facet;
+        $clone_filter_id = ListPresetFiltersBuilder::generateFilterId($facet_id, array_keys($facets));
+        $facets[$clone_filter_id] = $facet;
       }
     }
   }
@@ -162,47 +152,22 @@ class QuerySubscriber implements EventSubscriberInterface {
    * Preset values are always going to be in the query and cannot be removed.
    * Extra values can be included on top of the preset ones.
    *
-   * Since each facet can have multiple filters associated with it due to having
-   * multiple operators, we create a "fake" for each of these filters so we
-   * can
-   *
    * @param \Drupal\facets\FacetInterface $facet
    *   The facet.
    * @param \Drupal\oe_list_pages\ListPresetFilter $preset_filter
    *   The preset values.
    */
   protected function applyPresetFilterValues(FacetInterface $facet, ListPresetFilter $preset_filter): void {
-    $active_items = $facet->getActiveItems();
-
-    // Set the operator.
+    $facet->setActiveItems($preset_filter->getValues());
     if ($preset_filter->getOperator() === ListPresetFilter::NOT_OPERATOR) {
       $facet->setQueryOperator(ListPresetFilter::AND_OPERATOR);
       $facet->setExclude(TRUE);
-    }
-    else {
-      $facet->setExclude(FALSE);
-      $facet->setQueryOperator($preset_filter->getOperator());
-    }
 
-    if (empty($active_items)) {
-      // If the facet doesn't already have active items from this facet, we
-      // just set them all.
-      $facet->setActiveItems($preset_filter->getValues());
       return;
     }
 
-    // If the facet widget is not multiselect, it means we cannot merge the
-    // filter values with the preset, so we just override whatever is in the
-    // facet with the preset.
-    if ($facet->getWidget()['type'] !== 'oe_list_pages_multiselect') {
-      $facet->setActiveItems($preset_filter->getValues());
-      return;
-    }
-
-    // Otherwise, we have to merge them to ensure that the preset values are
-    // always set no matter what and to allow exposed filters to add to them.
-    $active_items = array_unique(array_merge($preset_filter->getValues(), $active_items));
-    $facet->setActiveItems($active_items);
+    $facet->setExclude(FALSE);
+    $facet->setQueryOperator($preset_filter->getOperator());
   }
 
 }
