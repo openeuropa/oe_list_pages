78864925ad93a9ebbba7c820b5621bee625948a7
upchuk <danny@webomelette.com>
2022-06-20 10:36:47 +0200

    EWPP-2371: Refactoring the list builder and execution manager to work with other config.

--- src/Plugin/ExtraField/Display/ListPageResults.php
+++ src/Plugin/ExtraField/Display/ListPageResults.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\oe_list_pages\Plugin\ExtraField\Display;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\oe_list_pages\ListPageConfiguration;
 
 /**
  * Extra field displaying the list page results.
@@ -28,7 +29,8 @@ class ListPageResults extends ListPageExtraFieldBase {
    * {@inheritdoc}
    */
   public function viewElements(ContentEntityInterface $entity) {
-    return [$this->listBuilder->buildList($entity)];
+    $configuration = ListPageConfiguration::fromEntity($entity);
+    return [$this->listBuilder->buildList($configuration)];
   }
 
 }
