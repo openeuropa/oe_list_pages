305ce80b6fa09e823683c83c88503156cac6be30
Julien Tekrane <julien.tekrane@laposte.net>
2023-08-18 15:56:17 +0000

    ELA-695: Facets checkbox widget.

---
+++ src/Plugin/facets/widget/MultiselectCheckboxWidget.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_list_pages\Plugin\facets\widget;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\facets\FacetInterface;
+use Drupal\facets\Result\Result;
+use Drupal\oe_list_pages\ListPresetFilter;
+
+/**
+ * The checkbox widget.
+ *
+ * @FacetsWidget(
+ *   id = "oe_list_pages_checkbox",
+ *   label = @Translation("List pages checkbox"),
+ *   description = @Translation("A checkbox filter widget"),
+ * )
+ */
+class MultiselectCheckboxWidget extends MultiselectWidget implements ContainerFactoryPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+   * @SuppressWarnings(PHPMD.NPathComplexity)
+   */
+  public function buildDefaultValueForm(array $form, FormStateInterface $form_state, FacetInterface $facet, ListPresetFilter $preset_filter = NULL): array {
+    $form = parent::buildDefaultValueForm($form, $form_state, $facet, $preset_filter);
+    // No need to set operator for checkboxes.
+    $form['oe_list_pages_filter_operator']['#access'] = FALSE;
+    $form['oe_list_pages_filter_operator']['#default_value'] = 'or';
+
+    $list_source = $form_state->get('list_source');
+    $plugin_id = $this->multiselectPluginManager->getPluginIdForFacet($facet, $list_source);
+    $filter_values = $preset_filter ? $preset_filter->getValues() : [];
+    $form[$facet->id()] = $form[$facet->id()][$plugin_id];
+    $form[$facet->id()]['#type'] = 'checkboxes';
+    $form[$facet->id()]['#default_value'] = array_filter($filter_values);
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareDefaultFilterValue(FacetInterface $facet, array $form, FormStateInterface $form_state): array {
+    echo (__FUNCTION__);
+    return [
+      'operator' => $form_state->getValue('oe_list_pages_filter_operator'),
+      'values' => $form_state->getValue($facet->id()) ?? [],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(FacetInterface $facet) {
+    $results = $facet->getResults();
+    $results = array_filter($results, function (Result $result) {
+      return $result->getDisplayValue() !== "";
+    });
+
+    $options = $this->transformResultsToOptions($results);
+
+    if ($options) {
+      $build[$facet->id()] = [
+        '#type' => 'checkboxes',
+        '#title' => $facet->getName(),
+        '#options' => $options,
+        '#default_value' => $facet->getActiveItems(),
+      ];
+    }
+
+    $build['#cache']['contexts'] = [
+      'url.query_args',
+      'url.path',
+    ];
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareValueForUrl(FacetInterface $facet, array &$form, FormStateInterface $form_state): array {
+    $values = $form_state->getValue($facet->id());
+    // Remove "0" values, unchecked checkboxes.
+    $values = array_filter($values);
+    return $values;
+  }
+
+}
