874918c04b3e0062d1476284ce8e0b4d1516520d
Julien Tekrane <julien.tekrane@laposte.net>
2023-09-21 17:08:54 +0000

    ELA-837: Not possible to use Default filter when Facets oe_list_pages_checkbox.

--- src/Plugin/facets/widget/MultiselectCheckboxWidget.php
+++ src/Plugin/facets/widget/MultiselectCheckboxWidget.php
@@ -23,42 +23,9 @@ class MultiselectCheckboxWidget extends MultiselectWidget implements ContainerFa
 
   /**
    * {@inheritdoc}
-   *
-   * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-   * @SuppressWarnings(PHPMD.NPathComplexity)
-   */
-  public function buildDefaultValueForm(array $form, FormStateInterface $form_state, FacetInterface $facet, ListPresetFilter $preset_filter = NULL): array {
-    $form = parent::buildDefaultValueForm($form, $form_state, $facet, $preset_filter);
-    // No need to set operator for checkboxes.
-    $form['oe_list_pages_filter_operator']['#access'] = FALSE;
-    $form['oe_list_pages_filter_operator']['#default_value'] = 'or';
-
-    $list_source = $form_state->get('list_source');
-    $plugin_id = $this->multiselectPluginManager->getPluginIdForFacet($facet, $list_source);
-    $filter_values = $preset_filter ? $preset_filter->getValues() : [];
-    $form[$facet->id()] = $form[$facet->id()][$plugin_id];
-    $form[$facet->id()]['#type'] = 'checkboxes';
-    $form[$facet->id()]['#default_value'] = array_filter($filter_values);
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function prepareDefaultFilterValue(FacetInterface $facet, array $form, FormStateInterface $form_state): array {
-    echo (__FUNCTION__);
-    return [
-      'operator' => $form_state->getValue('oe_list_pages_filter_operator'),
-      'values' => $form_state->getValue($facet->id()) ?? [],
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
    */
   public function build(FacetInterface $facet) {
     $results = $this->prepareResults($facet->getResults());
-
     $options = $this->transformResultsToOptions($results);
 
     if ($options) {
@@ -82,11 +49,25 @@ class MultiselectCheckboxWidget extends MultiselectWidget implements ContainerFa
    * {@inheritdoc}
    */
   public function prepareValueForUrl(FacetInterface $facet, array &$form, FormStateInterface $form_state): array {
-    return array_filter($form_state->getValue($facet->id()), function ($value) {
-      // Remove 0 (integer) values, that are unchecked checkboxes.
-      // "0" (string) can be a value (for boolean fields).
-      return $value !== 0;
-    });
+    $values = $form_state->getValue($facet->id());
+
+    if (!$values) {
+      return [];
+    }
+
+    if (!is_array($values)) {
+      return [$values];
+    }
+
+    // Remove 0 (integer) values, that are unchecked checkboxes.
+    // "0" (string) can be a value (for boolean fields).
+    $parameters = [];
+    foreach ($values as $key => $value) {
+      if($value !== 0) {
+        $parameters[] = $key;
+      }
+    }
+    return $parameters;
   }
 
 }
